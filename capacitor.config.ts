cdf654894d5f758451afe9f3a8e1a84f6451fece
YashAnetz <yashaa1897@gmail.com>
2025-01-04 02:53:09 -0500

    yes might be final

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'whatsapp-clone',
+  webDir: 'build'
+};
+
+export default config;
